438b8a905e8adcdcca030ca6a513d818f3c07783
Wim De Meester <wim.demeester@kuleuven.be>
2020-04-29 18:59:40 +0000

    Apply fixes from StyleCI

--- app/Instrument.php
+++ app/Instrument.php
@@ -1,15 +1,15 @@
 <?php
 
- /**
-  * Instrument eloquent model.
-  *
-  * PHP Version 7
-  *
-  * @category Instruments
-  * @author   Wim De Meester <deepskywim@gmail.com>
-  * @license  GPL3 <https://opensource.org/licenses/GPL-3.0>
-  * @link     http://www.deepskylog.org
-  */
+/**
+ * Instrument eloquent model.
+ *
+ * PHP Version 7
+ *
+ * @category Instruments
+ * @author   Wim De Meester <deepskywim@gmail.com>
+ * @license  GPL3 <https://opensource.org/licenses/GPL-3.0>
+ * @link     http://www.deepskylog.org
+ */
 
 namespace App;
 
